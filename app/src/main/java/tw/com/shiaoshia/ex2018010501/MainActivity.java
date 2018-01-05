b037f100110bd7ca87239b6ce1e04b27b1f96a20
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2018-01-05 10:41:56 +0800

    builder.setView

--- app/src/main/java/tw/com/shiaoshia/ex2018010501/MainActivity.java
+++ app/src/main/java/tw/com/shiaoshia/ex2018010501/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -39,6 +41,32 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this,"按下了HELP按鈕",Toast.LENGTH_SHORT).show();
             }
         });
+
         builder.show();
     }
+
+    public void click02(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("This is Title");
+        final EditText et = new EditText(MainActivity.this);
+        final TextView tv = (TextView)findViewById(R.id.textView);
+        builder.setView(et);
+        builder.setPositiveButton("確認", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this,"按下確認",Toast.LENGTH_SHORT).show();
+                tv.setText(et.getText().toString());
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this,"按下了取消",Toast.LENGTH_SHORT).show();
+                tv.setText("沒輸入資料");
+            }
+        });
+        builder.show();
+
+    }
+
 }
