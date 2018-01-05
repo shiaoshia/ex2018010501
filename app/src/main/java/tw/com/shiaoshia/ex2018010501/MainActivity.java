d686dcf18913ded432060bfbb7c7ff64917e286d
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2018-01-05 09:31:34 +0800

    AlertDialog.Builder

--- app/src/main/java/tw/com/shiaoshia/ex2018010501/MainActivity.java
+++ app/src/main/java/tw/com/shiaoshia/ex2018010501/MainActivity.java
@@ -1,7 +1,9 @@
 package tw.com.shiaoshia.ex2018010501;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void click01(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("This is Title");
+        builder.setMessage("Hello World!!");
+        builder.show();
     }
 }
