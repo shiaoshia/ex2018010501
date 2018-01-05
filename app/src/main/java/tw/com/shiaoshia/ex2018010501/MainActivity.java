c8cd0e16d2a802f5c7a31e29adeb3ccba60e9efe
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2018-01-05 10:00:33 +0800

    builder.setPositiveButton builder.setNegativeButton builder.setNeutralButton

--- app/src/main/java/tw/com/shiaoshia/ex2018010501/MainActivity.java
+++ app/src/main/java/tw/com/shiaoshia/ex2018010501/MainActivity.java
@@ -1,9 +1,11 @@
 package tw.com.shiaoshia.ex2018010501;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +21,24 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setTitle("This is Title");
         builder.setMessage("Hello World!!");
+        builder.setPositiveButton("確認", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this,"按了確認按鈕",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this,"按了取消按鈕",Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNeutralButton("HELP", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this,"按下了HELP按鈕",Toast.LENGTH_SHORT).show();
+            }
+        });
         builder.show();
     }
 }
